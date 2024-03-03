0b72b7302bc228a295699d167379ad754d23edd7
babos <babos@ChaDumYenTimes2>
2024-03-03 15:12:24 +0700

    Check missing

--- src/MainUser/Register.java
+++ src/MainUser/Register.java
@@ -114,6 +114,7 @@ public class Register extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
